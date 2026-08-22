f120a4000961480bc355e87e73ae5bff42325d33
Davi Abade <daviabade229@gmail.com>
2026-08-22 00:06:24 -0300

    fix: cara isso implementa regras de tamanho na validacao

--- src/validator.js
+++ src/validator.js
@@ -3,6 +3,14 @@ function validarChamado(titulo) {
         throw new Error("O título do chamado não pode ser vazio.");
     }
 
+    if (titulo.length < 5) {
+        throw new Error("o título deve possuir no mínimo 5 caracteres");
+    }
+
+    if (titulo.length > 100) {
+        throw new Error("o título deve possuir no máximo 100 caracteres");
+    }
+
     return true;
 }
 
